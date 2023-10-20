92b413e5234fb0b34053298bf7ae224b3b30fadb
Definitely-Human <odest66@gmail.com>
2023-10-20 18:15:06 +0200

    Init project.

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', '@typescript-eslint'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'always', jsx: 'always', ts: 'always', tsx: 'always' },
+    ],
+  },
+};
